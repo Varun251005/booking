354093eeca9715cd84d062e2289dcd38f608ab40
Varun251005 <Varun251005@users.noreply.github.com>
2026-04-25 11:34:53 +0530

    feat: add offers data with images for promotional display

---
+++ frontend/src/data/offers.js
@@ -0,0 +1,7 @@
+import offer1 from "../assets/offer1.png";
+import offer2 from "../assets/offer2.png";
+import offer3 from "../assets/offer3.png";
+
+const offers = [offer1, offer2, offer3];
+
+export default offers;
