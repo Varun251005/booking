00917661d00009cb70c8a9adfeb2817c3ec938c9
Varun Kumar <Varun251005@users.noreply.github.com>
2026-05-10 01:12:26 +0530

    fix(user): update default isVerified value to true in user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  isVerified: { type: Boolean, default: false },
-  name: String,
-  password: String,
-  role: { type: String, default: "user" },
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    isVerified: { type: Boolean, default: true },
+    name: String,
+    password: String,
+    role: { type: String, default: "user" },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
